165df418e574285c60d470a60a7d3127b7b01672
Andriy Golumbovsky <agolumbovsky@gmail.com>
2020-06-18 09:49:26 -0400

    stackoverflow snippet for use for netsh process

--- src/main.rs
+++ src/main.rs
@@ -15,3 +15,18 @@ fn main() {
         println!("your input was: {}", input);
     }
 }
+
+
+// following code is from nazioverflow
+// can be modified to call netsh process
+// -------------------------------------
+// use std::process::Command;
+
+// fn main() {
+//     let output = Command::new("echo")
+//         .arg("Hello world")
+//         .output()
+//         .expect("Failed to execute command");
+
+//     assert_eq!(b"Hello world\n", output.stdout.as_slice());
+// }
